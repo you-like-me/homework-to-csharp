7196e1945d1e4b04cf4ca963930581d3c2144547
katerina <you-like-me@yandex.ru>
2022-08-15 22:23:42 +0300

    Add Task 023

---
+++ Task_023/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число N ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень, в которую будет возводиться число N ");
+int a = int.Parse(Console.ReadLine());
+int index = 1;
+while (index <= Math.Abs(N))
+{
+    Console.Write(Math.Pow(index, a) + ", ");
+    index ++;
+}
