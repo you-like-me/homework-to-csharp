0d4957e8c062d0c5a44483ff895618b3cfcee6a2
katerina <you-like-me@yandex.ru>
2022-08-15 19:44:19 +0300

    Add Task 003

---
+++ Task_003/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите целое число n от 1 до 7 ");
+int n = Convert.ToInt32(Console.ReadLine());
+if((n ==0) ^ (n <= 7))
+{
+    if((n == 6) ^ (n == 7))
+    {
+        Console.WriteLine("Выходной");
+    
+    }
+    else
+    {
+        Console.WriteLine("Рабочий день");
+    }
+    
+}
+else
+{
+    Console.WriteLine("Ошибка: число не соответствует необходимым параметрам ");
+}    
