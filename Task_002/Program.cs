31a14d07d7aaa53d9973accd38c352340bb04350
katerina <you-like-me@yandex.ru>
2022-08-15 19:44:05 +0300

    Add Task 002

---
+++ Task_002/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите целое число a ");
+int a = Convert.ToInt32(Console.ReadLine());
+ 
+if(a >= 100)
+    {
+        if(a > 999)
+            {
+            int i = 10;
+            while (a / i > 1000)
+                {i = i * 10;}
+            Console.WriteLine("Третья цифра = " + (a / i) % 10);
+            }
+            else
+            {
+            Console.WriteLine("Третья цифра = " + a % 10);
+            }
+    }
+else
+    {   
+    Console.WriteLine("Третьей цифры нет");
+    }
