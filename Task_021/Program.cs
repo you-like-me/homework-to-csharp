80cd66bfca7b3ea59e5c3cf6e8486ceac1147549
katerina <you-like-me@yandex.ru>
2022-08-15 22:23:24 +0300

    Add Task 021

---
+++ Task_021/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите координаты точки А: ");
+int ax = int.Parse(Console.ReadLine());
+int ay = int.Parse(Console.ReadLine());
+int az = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B: ");
+int bx = int.Parse(Console.ReadLine());
+int by = int.Parse(Console.ReadLine());
+int bz = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Длина катета 1 = " + (ax - bx));
+Console.WriteLine("Длина катета 2 = " + (ay - by));
+Console.WriteLine("Длина катета 3 = " + (az - bz));
+
+double ABC = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2));
+
+Console.WriteLine("Расстояние между точками = " + ABC);
